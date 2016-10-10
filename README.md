e5c06c381f2b65ca0d7b465f05526e7660f77caa
Hrishikesh T <hrishikesh.t.i@gmail.com>
2016-10-11 04:01:50 +0530

    Add code for scatter plot of given data Add code for the least square fit straight line Add the output plot (lsq.png) Update README

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Week 2:
 
 1)Codes and figures of problem 1 of the assignment can be found in the week2 branch. Topics: Contours of a bivariate_gaussian, 2D Histogram of Gaussian distributed random variables and the Histogram of a function of these variables. The plots are present in gcontour.png, ghist2D.png and ghist1D.png and the code is present in gaussw2.py
 
-2)Codes and output screenshot and file of problem 2 have been added to the week2 branch. Code is present in pandasw2.py. The output file is panda_output.txt. 
+2)Codes and output screenshot and file of problem 2 have been added to the week2 branch. Code is present in pandasw2.py. The output file is panda_output.txt.
+
+3)Code and output for 3a) and 3b) have been added to the week2 branch. Code is present in linear_expansion.py and the least square fit line in lsq.png.
--- linear_expansion.py
+++ linear_expansion.py
@@ -4,14 +4,68 @@ import matplotlib.pyplot as plt
 #data is an array whose each element is 2-array
 data = np.genfromtxt('linearexpansion.csv', delimiter = ',')
 #create array for temperature and length
-temp = []
-length = []
-for point in range(len(data)):
-    temp.append(float(data[point][0]))
-    length.append(float(data[point][1]))
+temps = []
+lengths = []
+num_of_data_pts = len(data)
+for point in range(num_of_data_pts):
+    temps.append(float(data[point][0]))
+    lengths.append(float(data[point][1]))
+#Finding the maximum and minimum values of length:
+#This will help in defining a range for m and c
+#Length = m(temp) + c
+#We know in general that length increases with temperature.
+#So, the max_index is most likely to be lying among the last 1/3rd indices.
+#Similarly, the min_index is most likely to be among the first 1/3rd indices.
+#Even if they don't, we still get a very good estimate for m because m won't be among the extreme values. We just need a  good range to search in.
+
+#Considering the last 1/3rd indices:
+max_index = int((num_of_data_pts*2)/3)
+for l in range(int((num_of_data_pts*2)/3),num_of_data_pts):
+#(num_of_data_pts*2)/3 will always be an integer(due to truncation), so we are safe.
+    if lengths[l] > lengths[max_index]:
+        max_index = l
+#Considering the first 1/3rd indices:
+min_index = 0
+for l in range(int(num_of_data_pts/3)):
+    if lengths[l] < lengths[min_index]:
+        min_index = l
+#max_index and min_index now store the indices of the max and min lengths respectively.
+#Line joining point with minimum slope
+max_slope = (lengths[max_index] - lengths[min_index])/(temps[max_index] - temps[min_index])
+#min_slope will be zero. We know that length increases with temperature.
+
+#line joing max and min points will have the least(including sign) intercept.
+#for y = mx +c passing through (x1, y1) and (x2, y2), c = (x2y1-x1y2)/(x2-x1)
+min_c = (temps[max_index]*lengths[min_index] - temps[min_index]*lengths[max_index])/(temps[max_index] - temps[min_index])
+#max_c will correspond to the y co-ordinate of the maximum point.
+max_c = lengths[max_index]
+M = np.linspace(0, max_slope, 250)
+C = np.linspace(min_c, max_c, 250)
+m_best = 0.0
+c_best = 0.0
+#Initialising Least Square distance (LSq)to the worst possible condition
+LSq = num_of_data_pts*((lengths[max_index] - min_c)**2)
+i = 0
+for m in M:
+    for c in C:
+        lsq = 0.0
+        for l in range(num_of_data_pts):
+            lsq = lsq + ((lengths[l] - (m*temps[l] + c))**2)
+        if lsq < LSq:
+            m_best = m
+            c_best = c
+            LSq = lsq
+
 plt.figure(1)
 plt.title("Scatter plot of lengths vs temperature")
-plt.plot(temp, length, 'ro')
+plt.plot(temps, lengths, 'ro')
 plt.xlabel("Temperature ("+r"$^0C$)")
 plt.ylabel("Length (mm)")
+bbox_props1 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
+plt.text(0.75, 1250, "slope : "+str(m_best)+" and intercept : "+str(c_best), size = 14, bbox = bbox_props1)
+plt.plot([0, 10], [c_best, (m_best*10 + c_best)], color = 'g', linewidth = 2.0)
+bbox_props2 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
+plt.text(3.5, 950, "At 15"+r"$^oC$, length = "+str(15*m_best + c_best)+"mm", size = 14, bbox = bbox_props2)
+print("Linear extrapolation upto 15"+r"$^oC$ gives an expected length of "+str(15*m_best + c_best)+"mm")
+#Linear extrapolation upto 15$^oC$ gives an expected length of 1336.96991312mm
 plt.show()
