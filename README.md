a78f43a4091e9de81f40317957f2856c52f4c40a
Hrishikesh T <hrishikesh.t.i@gmail.com>
2016-10-12 00:18:05 +0530

    Add more detailed comments Update README

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ This repository contains code for the EP 219 Fall 2016 Assignment.
 
 Week 2:
 
-1)Codes and figures of problem 1 of the assignment can be found in the week2 branch. Topics: Contours of a bivariate_gaussian, 2D Histogram of Gaussian distributed random variables and the Histogram of a function of these variables. The plots are present in gcontour.png, ghist2D.png and ghist1D.png and the code is present in gaussw2.py
+1)Codes and figures of problem 1 of the assignment can be found in the analytical branch. Topics: Contours of a bivariate_gaussian, 2D Histogram of Gaussian distributed random variables and the Histogram of a function of these variables. The plots are present in gcontour.png, ghist2D.png and ghist1D.png and the code is present in gaussw2.py
 
-2)Codes and output screenshot and file of problem 2 have been added to the week2 branch. Code is present in pandasw2.py. The output file is panda_output.txt.
+2)Codes and output screenshot and file of problem 2 have been added to the analytical branch. Code is present in pandasw2.py. The output file is panda_output.txt.
 
-3)Code and output for 3a) and 3b) have been added to the week2 branch. Code is present in linear_expansion.py and the least square fit line in lsq.png.
+3)Code and output for 3a), 3b) and 3c) have been added to the analytical branch. Code is present in linear_expansion.py and the least square fit line in lsq.png. Note that this branch uses the analytical formulae to estimate the parameters and errors. 
--- linear_expansion.py
+++ linear_expansion.py
@@ -66,35 +66,42 @@ for length in lengths:
     length_sum = length_sum + length
 mean_temp = temp_sum/num_of_data_pts
 mean_length = length_sum/num_of_data_pts
+#Summation temp_i*length_i:
 sum_ti_li = 0.0
+#Summation (temp_i)^2:
 sum_ti2 = 0.0
 for i in range(num_of_data_pts):
     sum_ti_li = sum_ti_li + temps[i]*lengths[i]
     sum_ti2 = sum_ti2 + (temps[i]**2)
+#The derivation of the following two formulae for m_best and c_best can be found in the report
 m_best = (sum_ti_li - mean_temp*length_sum)/(sum_ti2 - num_of_data_pts*(mean_temp**2))
 c_best = mean_length - m_best*mean_temp
-
+#Making the scatter plot with the best-fit line
 plt.figure(1)
 plt.title("Scatter plot of lengths vs temperature")
 plt.plot(temps, lengths, 'ro')
 plt.xlabel("Temperature ("+r"$^0C$)")
 plt.ylabel("Length (mm)")
+#Text_box with m_best and c_best values
 bbox_props1 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
 plt.text(0.20, 1275, "slope : "+str(m_best)+r"$\frac{mm}{^0C}$"+" and intercept : "+str(c_best)+r"$mm$", size = 10, bbox = bbox_props1)
+#Plot the best-fit line
 plt.plot([0, 10], [c_best, (m_best*10 + c_best)], color = 'g', linewidth = 2.0)
+#Text_box with extrapolated length
 bbox_props2 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
 plt.text(3.5, 950, "At 15"+r"$^0C$"+", length = "+str(15*m_best + c_best)+"mm", size = 12, bbox = bbox_props2)
 print("Linear extrapolation upto 15"+"ºC"+" gives an expected length of "+str(15*m_best + c_best)+"mm")
-print("m is "+str(m_best))
-print("c is "+str(c_best))
-#Linear extrapolation upto 15$^oC$ gives an expected length of 1337.0421541416586
+print("m_best is "+str(m_best))
+print("c_best is "+str(c_best))
+#Linear extrapolation upto 15ºC gives an expected length of 1337.0421541416586
 #Calculate single measurement error of lis:
 sigma_li2 = 0.0
-t_ext = 15 #degree C
+t_ext = 15 #degree Celsius
 #First we technically calculate sum of squares of deviations from sample mean
 for length in lengths:
     sigma_li2 = sigma_li2 + ((length - mean_length)**2)
 sigma_li2 = sigma_li2/(num_of_data_pts-2)
+#These formulae have been explained in the report
 sigma2_y_ext = sigma_li2*((1.0/num_of_data_pts)+((mean_temp**2)+(t_ext**2))/(sum_ti2 - num_of_data_pts*(mean_temp**2)))
 sigma_y_ext = math.sqrt(sigma2_y_ext)
 sigma_li = math.sqrt(sigma_li2)
