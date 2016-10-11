c8d20c386c10433d90b9781db4d1dc9dff8dfcbe
Hrishikesh T <hrishikesh.t.i@gmail.com>
2016-10-11 23:09:09 +0530

    Add code for error in extrapolatedd length. Update image to reflect use of analytical method.

--- linear_expansion.py
+++ linear_expansion.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 #import data from the file and store it in data
 #data is an array whose each element is 2-array
 data = np.genfromtxt('linearexpansion.csv', delimiter = ',')
@@ -10,6 +11,7 @@ num_of_data_pts = len(data)
 for point in range(num_of_data_pts):
     temps.append(float(data[point][0]))
     lengths.append(float(data[point][1]))
+'''
 #Finding the maximum and minimum values of length:
 #This will help in defining a range for m and c
 #Length = m(temp) + c
@@ -39,8 +41,8 @@ max_slope = (lengths[max_index] - lengths[min_index])/(temps[max_index] - temps[
 min_c = (temps[max_index]*lengths[min_index] - temps[min_index]*lengths[max_index])/(temps[max_index] - temps[min_index])
 #max_c will correspond to the y co-ordinate of the maximum point.
 max_c = lengths[max_index]
-M = np.linspace(0, max_slope, 500)
-C = np.linspace(min_c, max_c, 500)
+M = np.linspace(0, max_slope, 250)
+C = np.linspace(min_c, max_c, 250)
 m_best = 0.0
 c_best = 0.0
 #Initialising Least Square distance (LSq)to the worst possible condition
@@ -55,6 +57,22 @@ for m in M:
             m_best = m
             c_best = c
             LSq = lsq
+'''
+temp_sum = 0.0
+for temp in temps:
+    temp_sum = temp_sum + temp
+length_sum = 0.0
+for length in lengths:
+    length_sum = length_sum + length
+mean_temp = temp_sum/num_of_data_pts
+mean_length = length_sum/num_of_data_pts
+sum_ti_li = 0.0
+sum_ti2 = 0.0
+for i in range(num_of_data_pts):
+    sum_ti_li = sum_ti_li + temps[i]*lengths[i]
+    sum_ti2 = sum_ti2 + (temps[i]**2)
+m_best = (sum_ti_li - mean_temp*length_sum)/(sum_ti2 - num_of_data_pts*(mean_temp**2))
+c_best = mean_length - m_best*mean_temp
 
 plt.figure(1)
 plt.title("Scatter plot of lengths vs temperature")
@@ -62,12 +80,24 @@ plt.plot(temps, lengths, 'ro')
 plt.xlabel("Temperature ("+r"$^0C$)")
 plt.ylabel("Length (mm)")
 bbox_props1 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
-plt.text(0.20, 1275, "slope : "+str(m_best)+r"$\frac{mm}{^0C}$"+" and intercept : "+str(c_best)+r"$mm$", size = 14, bbox = bbox_props1)
+plt.text(0.20, 1275, "slope : "+str(m_best)+r"$\frac{mm}{^0C}$"+" and intercept : "+str(c_best)+r"$mm$", size = 10, bbox = bbox_props1)
 plt.plot([0, 10], [c_best, (m_best*10 + c_best)], color = 'g', linewidth = 2.0)
 bbox_props2 =  dict(boxstyle = "square, pad=0.4", fc = "w", ec = 'k', lw = 1)
-plt.text(3.5, 950, "At 15"+r"$^0C$"+", length = "+str(15*m_best + c_best)+"mm", size = 14, bbox = bbox_props2)
+plt.text(3.5, 950, "At 15"+r"$^0C$"+", length = "+str(15*m_best + c_best)+"mm", size = 12, bbox = bbox_props2)
 print("Linear extrapolation upto 15"+"ºC"+" gives an expected length of "+str(15*m_best + c_best)+"mm")
 print("m is "+str(m_best))
 print("c is "+str(c_best))
-#Linear extrapolation upto 15$^oC$ gives an expected length of 1336.96991312mm
+#Linear extrapolation upto 15$^oC$ gives an expected length of 1337.0421541416586
+#Calculate single measurement error of lis:
+sigma_li2 = 0.0
+t_ext = 15 #degree C
+#First we technically calculate sum of squares of deviations from sample mean
+for length in lengths:
+    sigma_li2 = sigma_li2 + ((length - mean_length)**2)
+sigma_li2 = sigma_li2/(num_of_data_pts-2)
+sigma2_y_ext = sigma_li2*((1.0/num_of_data_pts)+((mean_temp**2)+(t_ext**2))/(sum_ti2 - num_of_data_pts*(mean_temp**2)))
+sigma_y_ext = math.sqrt(sigma2_y_ext)
+sigma_li = math.sqrt(sigma_li2)
+print("Error in li is " + str(sigma_li)+" mm"+"\n")
+print("Error in Length at 15ºC is "+str(sigma_y_ext)+" mm")
 plt.show()
